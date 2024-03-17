90902908c0302a9ee40820cfaa638f5ea6a97f40
mariyasada <mariyasada2406@gmail.com>
2024-03-17 12:38:29 +0530

    remove some unnecessary code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async (req, res, next) => {
-  console.log(req.body, "check data");
+const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "first api request" });
 });
 
